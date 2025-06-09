6ddbff8d3c8ad376392b338a47e207230c5a5562
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-06-09 17:45:24 +0700

    tutup file

--- iofile.cpp
+++ iofile.cpp
@@ -38,6 +38,9 @@ int main ()
         {
         // dan tampilkan disini
         cout << baris << '\n';
+    
         }
+        // tutup file tersebut setelah selesai
+        infile.close();
     }
 }
